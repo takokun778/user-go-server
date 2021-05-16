73fc32c183179720bf424f7cf71426c214d872e1
takokun778 <takokun.778@gmail.com>
2021-05-16 17:16:32 +0900

    fix UserRepositoryの引数と返り値を修正

--- domains/repositories/user_repository.go
+++ domains/repositories/user_repository.go
@@ -3,9 +3,9 @@ package repositories
 import "github.com/takokun778/user-go-server/domains/models"
 
 type UserRepository interface {
-	Save(models.User) (models.User, error)
-	Find(id string) (models.User, error)
-	FindAll() ([]models.User, error)
-	Update(models.User) (models.User, error)
-	Delete(models.User) error
+	Save(user *models.User) (*models.User, error)
+	Find(id string) (*models.User, error)
+	FindAll() ([]*models.User, error)
+	Update(user *models.User) (*models.User, error)
+	Delete(id string) error
 }
