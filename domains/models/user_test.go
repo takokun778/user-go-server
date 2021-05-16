96db2ae3b37590a6c811064b154c85a907cef13d
takokun778 <takokun.778@gmail.com>
2021-05-16 16:52:15 +0900

    test uuidが生成されることを確認するテストを追加

--- domains/models/user_test.go
+++ domains/models/user_test.go
@@ -1,10 +1,21 @@
 package models
 
 import (
+	"regexp"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewUser(t *testing.T) {
-	result := NewUser("大園玲")
+	name := "大園玲"
+	result := NewUser(name)
+	assert.Equal(t, result.name, name)
+	assert.True(t, isValidUUID(result.id))
 	t.Log(result)
+}
+
+func isValidUUID(uuid string) bool {
+	r := regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$")
+	return r.MatchString(uuid)
 }
