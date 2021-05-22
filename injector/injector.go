a0e4aac3a69500bc9166924ffe5ef47e67bb4be0
takokun778 <takokun.778@gmail.com>
2021-05-22 11:19:16 +0900

    style injectorにコメントを追加

--- injector/injector.go
+++ injector/injector.go
@@ -7,26 +7,11 @@ import (
 	"github.com/takokun778/user-go-server/usecases"
 )
 
-type UseCase struct {
-	create *usecases.UserCreateUseCase
-}
-
-func newUseCase(userRepository repositories.UserRepository) (usecase *UseCase) {
-	usecase = new(UseCase)
-	usecase.create = usecases.NewUserCreateUseCase(userRepository)
-	return
-}
-
-type Controller struct {
-	Create *controllers.UserCreateController
-}
-
-func newController(usecase *UseCase) (controller *Controller) {
-	controller = new(Controller)
-	controller.Create = controllers.NewUserCreateController(*usecase.create)
-	return
-}
-
+/*
+ DI
+ 依存性を解決するクラス
+ serverクラスが肥大化するのを避けるため利用
+*/
 type Injector struct {
 	Controller *Controller
 }
@@ -34,7 +19,33 @@ type Injector struct {
 func NewInjector() (injector *Injector) {
 	injector = new(Injector)
 	// repositoryの依存性解決
+	// この部分でリポジトリーを付け替えられるようにしている
+	// TODO 埋め込むのではなく引数に宣言すべき？ -> テストで使える
 	usecase := newUseCase(gateways.NewUserGateway())
 	injector.Controller = newController(usecase)
+	return
+}
+
+// コントローラを集約したクラス
+type Controller struct {
+	Create *controllers.UserCreateController
+}
+
+// コントローラコンストラクタ
+func newController(usecase *UseCase) (controller *Controller) {
+	controller = new(Controller)
+	controller.Create = controllers.NewUserCreateController(*usecase.create)
+	return
+}
+
+// ユースケースを集約したクラス
+type UseCase struct {
+	create *usecases.UserCreateUseCase
+}
+
+// ユースケースコンストラクタ
+func newUseCase(userRepository repositories.UserRepository) (usecase *UseCase) {
+	usecase = new(UseCase)
+	usecase.create = usecases.NewUserCreateUseCase(userRepository)
 	return
 }
